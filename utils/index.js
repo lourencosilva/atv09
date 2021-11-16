b031b46e60f34dab42ed960b26908716317102de
Lourenço Silva <lourenco.silva72@gmail.com>
2021-11-16 20:34:15 -0300

    Delete utils directory

--- utils/index.js
+++
@@ -1,8 +0,0 @@
-import Tabs from "./../Navigation/tabs"
-import Login from "./../Login"
-import Book from "./../Book"
-import User from './../User';
-import Storage from './../../aula05/Storage';
-import Home from "./../Home"
-
-export { Tabs, Login, Book, User, Storage, Home }
